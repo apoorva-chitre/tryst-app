3ae09f5ec13748c1f2327698672cef0f101ee4ff
Apoorva Chitre <apoorvac09@gmail.com>
2020-04-05 16:36:23 -0400

    project setup, logo, favicon and initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppointmentsComponent } from './appointments/appointments.component';
+import { MeetingsComponent } from './meetings/meetings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppointmentsComponent,
+    MeetingsComponent
   ],
   imports: [
     BrowserModule
